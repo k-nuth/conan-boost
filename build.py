ea581adb9387ee0ffddd25bcbcdcfc14d849b755
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-28 12:30:49 -0300

    fix

--- build.py
+++ build.py
@@ -1,13 +1,18 @@
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+
+
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
-    builder.add_common_builds(shared_option_name="libpng:shared", pure_c=True)
+    builder.add_common_builds(shared_option_name="boost:shared")
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
-                and not options["libpng:shared"]:
+                and not options["boost:shared"]:
 
             filtered_builds.append([settings, options, env_vars, build_requires])
 
