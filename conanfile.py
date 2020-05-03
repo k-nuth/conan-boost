0615560cb98c189a2be6860ab82a7c07930a3439
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-05-03 11:28:05 -0300

    version 1.73.0

--- conanfile.py
+++ conanfile.py
@@ -40,7 +40,7 @@ class KnuthConanBoost(KnuthConanFile):
         return os.path.dirname(os.path.abspath(__file__))
 
     name = "boost"
-    version = "1.72.0"
+    version = "1.73.0"
 
     settings = "os", "arch", "compiler", "build_type"
     folder_name = "boost_%s" % version.replace(".", "_")
