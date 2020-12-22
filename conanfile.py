d1de09a95da9ec7e7c5f59b910a5231a50d04346
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-12-22 16:01:23 -0300

    uses ICU 68.2

--- conanfile.py
+++ conanfile.py
@@ -185,8 +185,9 @@ class KnuthConanBoost(KnuthConanFile):
             #TODO(fernando): what about fPIC?
 
         if self.options.use_icu and self.use_icu:
-            self.requires("icu/64.2@kth/stable")
+            # self.requires("icu/64.2@kth/stable")
             # self.requires("icu/67.1@kth/stable")
+            self.requires("icu/68.2@kth/stable")
             self.options["icu"].shared = self.is_shared #False
             #TODO(fernando): what about fPIC?
 
